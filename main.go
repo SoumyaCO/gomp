fac29f54a83e538e13703d650afd908dc958fabf
Soumyadip Bhattacharjya <101652501+SoumyaCO@users.noreply.github.com>
2024-09-18 10:12:24 +0530

    update: playing in another goroutine

--- main.go
+++ main.go
@@ -2,66 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/gopxl/beep"
-	// "github.com/gopxl/beep/effects"
 	"github.com/gopxl/beep/mp3"
 	"github.com/gopxl/beep/speaker"
-
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
-
-const listHeight = 14
-const pattern = "*.mp3"
-
-var (
-	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
-)
-
-type item string
-
-func (i item) FilterValue() string { return "" }
-
-type itemDelegate struct{}
-
-func (d itemDelegate) Height() int                             { return 1 }
-func (d itemDelegate) Spacing() int                            { return 0 }
-func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
-		return
-	}
-
-	str := fmt.Sprintf("%d. %s", index+1, i)
-
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
-}
 
 type model struct {
-	list     list.Model
-	choice   string
-	quitting bool
+	items         []string
+	selected      int
+	isPlaying     bool
+	closingSignal chan struct{}
 }
 
 func (m model) Init() tea.Cmd {
@@ -70,98 +26,99 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.list.SetWidth(msg.Width)
-		return m, nil
 
 	case tea.KeyMsg:
-		switch keypress := msg.String(); keypress {
-		case "q", "ctrl+c":
-			m.quitting = true
+		switch msg.String() {
+		case "q":
 			return m, tea.Quit
 
-		case "enter":
-			i, ok := m.list.SelectedItem().(item)
-			if ok {
-				m.choice = string(i)
+		case "k":
+			if m.selected > 0 {
+				m.selected--
 			}
-			return m, tea.Quit
+
+		case "j":
+			if m.selected < len(m.items)-1 {
+				m.selected++
+			}
+
+		case "enter":
+			if m.isPlaying {
+				m.closingSignal <- struct{}{} // Stop currently playing music
+			}
+			m.isPlaying = true
+			go m.playMusic(m.items[m.selected])
+
 		}
 	}
 
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
+	return m, nil
 }
 
 func (m model) View() string {
-	if m.choice != "" {
-		f, err := os.Open(m.choice)
-		streamer, format, err := mp3.Decode(f)
-		if err != nil {
-			log.Fatalf("Error in Playing %v \n", err)
+	s := "Choose a song:\n\n"
+
+	for i, item := range m.items {
+		cursor := " " // no cursor
+		if m.selected == i {
+			cursor = ">" // current cursor
 		}
-		defer streamer.Close()
-
-		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
-		ctrl := &beep.Ctrl{Streamer: streamer, Paused: false}
-
-		speaker.Play(ctrl)
-		for {
-			fmt.Print("Press [Enter] to pause/resume. ")
-			fmt.Scanln()
-
-			speaker.Lock()
-			ctrl.Paused = !ctrl.Paused
-			speaker.Unlock()
-		}
-
-		// return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice))
+		s += fmt.Sprintf("%s %s\n", cursor, item)
 	}
-	if m.quitting {
-		return quitTextStyle.Render("Not hungry? That’s cool.")
+
+	if m.isPlaying {
+		s += "\nPlaying music...\n"
 	}
-	return "\n" + m.list.View()
+
+	s += "\nPress q to quit.\n"
+	return s
+}
+
+func (m *model) playMusic(filepath string) {
+	// Load music file
+	f, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println("Could not open file:", err)
+		return
+	}
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		fmt.Println("Could not decode file:", err)
+		return
+	}
+	defer streamer.Close()
+
+	// Initialize the speaker with the sample rate
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	select {
+	case <-done:
+		m.isPlaying = false
+	case <-m.closingSignal:
+		speaker.Clear()
+		m.isPlaying = false
+	}
 }
 
 func main() {
-	items := []list.Item{}
-	files, err := filepath.Glob(pattern)
+	items, err := filepath.Glob("*.mp3")
 	if err != nil {
-		log.Fatalf("Can't get %s files: %v", pattern, err)
+		log.Fatal("error listing files")
 	}
 
-	for _, file := range files {
-		items = append(items, item(file))
+	m := model{
+		items:     items,
+		done:      make(chan struct{}),
+		isPlaying: false,
 	}
-	// items := []list.Item{
-	// 	item("Ramen"),
-	// 	item("Tomato Soup"),
-	// 	item("Hamburgers"),
-	// 	item("Cheeseburgers"),
-	// 	item("Currywurst"),
-	// 	item("Okonomiyaki"),
-	// 	item("Pasta"),
-	// 	item("Fillet Mignon"),
-	// 	item("Caviar"),
-	// 	item("Just Wine"),
-	// }
-
-	const defaultWidth = 20
-
-	l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
-	l.Title = "What do you want for dinner?"
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-	l.Styles.HelpStyle = helpStyle
-
-	m := model{list: l}
 
 	if _, err := tea.NewProgram(m).Run(); err != nil {
-		fmt.Println("Error running program:", err)
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 }
